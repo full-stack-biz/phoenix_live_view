b03bfdc83d4c25973cf406469b41497650cb2650
Sergey Moiseev <moiseev.sergey@gmail.com>
2020-05-04 20:47:32 +0300

    Work in progress

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -9,8 +9,12 @@ module.exports = {
     libraryTarget: 'umd',
     globalObject: 'this'
   },
+   // Enable sourcemaps for debugging webpack's output.
+   devtool: "source-map",
   module: {
     rules: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+      { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
       {
         test: path.resolve(__dirname, './js/phoenix_live_view.js'),
         use: [{
@@ -24,7 +28,9 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
-      }
+      },
+        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+        { test: /\.js$/, loader: "source-map-loader" }
     ]
   },
   plugins: []
